4e4b5fb791f8161d68d54c36c1309ba6439e615e
Faysal Badaoui <faysalbadaoui02@gmail.com>
2024-05-04 12:00:30 +0200

    start endpoint to create and process metadata

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3001;
 
-// Middleware to parse JSON
 app.use(bodyParser.json());
 
-// POST endpoint to process image and upload to IPFS
+// POST endpoint to create NFT image and upload to IPFS
 app.post('/process-image', async (req, res) => {
     try {
         const { ownerWallet, igpostId } = req.body;
@@ -17,6 +16,27 @@ app.post('/process-image', async (req, res) => {
         }
 
         const imagePath = await addTextAndOverlay('background.jpg', ownerWallet, igpostId);
+        const ipfsResult = await uploadToIPFS(imagePath);
+        
+        res.send({
+            message: 'Image processed and uploaded successfully',
+            data: ipfsResult
+        });
+    } catch (error) {
+        console.error('Error processing image:', error);
+        res.status(500).send({ error: 'Failed to process image and upload to IPFS' });
+    }
+});
+
+// POST endpoint to process metadata and upload to IPFS
+app.post('/process-metadata', async (req, res) => {
+    try {
+        const { imageHash, igpostId } = req.body;
+        if (!imageHash || !igpostId) {
+            return res.status(400).send({ error: 'Missing required parameters: hash of the image or igpostId' });
+        }
+
+        const imagePath = await createMetadata(imageHash, igpostId);
         const ipfsResult = await uploadToIPFS(imagePath);
         
         res.send({
