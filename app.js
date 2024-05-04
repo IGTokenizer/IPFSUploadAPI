15280cee5289bfde65a10222933a7f52be434fe7
Faysal Badaoui <faysalbadaoui02@gmail.com>
2024-05-04 11:03:20 +0200

    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { addTextAndOverlay, uploadToIPFS } = require('./imageProcessing'); // Ass
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Middleware to parse JSON
 app.use(bodyParser.json());
